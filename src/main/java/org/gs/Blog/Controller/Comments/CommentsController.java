b4e853e9787797fbbeab58c85a3dffdf6078cd69
shikhar8 <shikhar.suman619@gmail.com>
2020-05-25 15:33:22 +0530

    "adding classes and application.properties"

--- src/main/java/org/gs/Blog/Controller/Comments/CommentsController.java
+++ src/main/java/org/gs/Blog/Controller/Comments/CommentsController.java
@@ -12,18 +12,18 @@ import java.util.List;
 public class CommentsController {
     @Autowired
     private CommentsService commentsService;
-    @RequestMapping(name = "/comments/{id}",method = RequestMethod.GET)
+    @RequestMapping(value = "/comments/{id}",method = RequestMethod.GET)
     public List<Comment> getCommentsById(@PathVariable int id){
         return commentsService.getCommentsById(id);
     }
-    @RequestMapping(name = "/comments",method = RequestMethod.POST)
+    @RequestMapping(value = "/comments",method = RequestMethod.POST)
     void addPost(@RequestBody Comment comment){
         commentsService.addComment(comment);
     }
-    @RequestMapping(name = "/comments/{id}",method = RequestMethod.PUT)
+    @RequestMapping(value = "/comments/{id}",method = RequestMethod.PUT)
     void updatePost(@RequestBody Comment comment,@PathVariable int id){
          commentsService.updateComment(comment,id);
-    }@RequestMapping(name = "/comments/{id}",method = RequestMethod.DELETE)
+    }@RequestMapping(value = "/comments/{id}",method = RequestMethod.DELETE)
     void deletePost(@PathVariable int id){
          commentsService.deleteComment(id);
     }
--- src/main/java/org/gs/Blog/Controller/Posts/PostsController.java
+++ src/main/java/org/gs/Blog/Controller/Posts/PostsController.java
@@ -12,22 +12,22 @@ import java.util.List;
 public class PostsController {
     @Autowired
     private PostsService postsService;
-    @RequestMapping(name = "/posts",method = RequestMethod.GET)
+    @RequestMapping(value = "/posts",method = RequestMethod.GET)
     public List<Post> getPosts(){
         return postsService.getPosts();
     }
-   @RequestMapping(name = "/posts/{id}",method = RequestMethod.GET)
+   @RequestMapping(value = "/posts/{id}",method = RequestMethod.GET)
     public Post getPostsById(@PathVariable int id){
         return postsService.getPostsById(id);
     }
-    @RequestMapping(name = "/posts",method = RequestMethod.POST)
+    @RequestMapping(value = "/posts",method = RequestMethod.POST)
     void addPost(@RequestBody Post post){
          postsService.addPost(post);
     }
-    @RequestMapping(name = "/posts/{id}",method = RequestMethod.PUT)
+    @RequestMapping(value = "/posts/{id}",method = RequestMethod.PUT)
     void updatePost(@RequestBody Post post,@PathVariable int id){
          postsService.updatePost(post,id);
-    }@RequestMapping(name = "/posts/{id}",method = RequestMethod.DELETE)
+    }@RequestMapping(value = "/posts/{id}",method = RequestMethod.DELETE)
     void deletePost(@PathVariable int id){
          postsService.deletePost(id);
     }
