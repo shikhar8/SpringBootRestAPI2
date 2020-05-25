38a8a4ffef5d3d915b88de1fa044be6b2daa973b
shikhar8 <shikhar.suman619@gmail.com>
2020-05-25 12:40:55 +0530

    "adding Classes"

--- src/main/java/org/gs/Blog/Service/Comments/CommentsService.java
+++ src/main/java/org/gs/Blog/Service/Comments/CommentsService.java
@@ -1,21 +1,30 @@
 package org.gs.Blog.Service.Comments;
 
 import org.gs.Blog.Entity.Comment.Comment;
+import org.gs.Blog.Repository.Comments.CommentsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class CommentsService {
+    @Autowired
+    private CommentsRepository commentsRepository;
     public List<Comment> getCommentsById(int id) {
         return null;
     }
 
     public void addComment(Comment comment) {
+        commentsRepository.save(comment);
     }
 
     public void updateComment(Comment comment, int id) {
+        commentsRepository.save(comment);
     }
 
     public void deleteComment(int id) {
+        Comment comment=commentsRepository.findById(id).get();
+        if(comment!=null)
+            commentsRepository.delete(comment);
     }
 }
--- src/main/java/org/gs/Blog/Service/Posts/PostsService.java
+++ src/main/java/org/gs/Blog/Service/Posts/PostsService.java
@@ -1,10 +1,8 @@
 package org.gs.Blog.Service.Posts;
-
 import org.gs.Blog.Entity.Post.Post;
 import org.gs.Blog.Repository.Posts.PostsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 @Service
